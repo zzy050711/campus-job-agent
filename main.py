386dfbfbbd71fa7ad3ad2d1232171ba317863aa9
zzy050711 <1346586820@qq.com>
2026-09-01 15:59:47 +0800

    完成FastAPI基础服务

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("欢迎来到 Campus Job Agent！")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "欢迎来到 Campus Job Agent！"
+    }
+
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
