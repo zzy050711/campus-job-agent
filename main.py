25fae28e5ff0135d179765d532c8586557b77046
zzy050711 <1346586820@qq.com>
2026-09-02 09:43:19 +0800

    完成前端页面开发

--- main.py
+++ main.py
@@ -1,17 +1,75 @@
+import os
+import json
+
+from dotenv import load_dotenv
+from openai import OpenAI
 from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 
+# 读取 .env
+load_dotenv()
+
+# 获取 API Key
+api_key = os.getenv("DEEPSEEK_API_KEY")
+
+# 创建 DeepSeek 客户端
+client = OpenAI(
+    api_key=api_key,
+    base_url="https://api.deepseek.com"
+)
+
+# 创建 FastAPI 应用
 app = FastAPI()
-
-
 @app.get("/")
 def home():
-    return {
-        "message": "欢迎来到 Campus Job Agent！"
-    }
+    return FileResponse("index.html")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# 定义用户输入的数据格式
+class UserRequest(BaseModel):
+    question: str
 
 
-@app.get("/health")
-def health():
-    return {
-        "status": "ok"
-    }
+# 创建 /analyze 接口
+@app.post("/analyze")
+def analyze(request: UserRequest):
+
+    response = client.chat.completions.create(
+        model="deepseek-v4-flash",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+你是一名专业的校园求职顾问。
+
+当用户询问求职问题时，请严格按照以下 JSON 格式回答：
+
+{
+    "适合岗位": ["岗位1", "岗位2", "岗位3"],
+    "原因": "简要说明为什么适合",
+    "技能缺口": ["技能1", "技能2", "技能3"],
+    "建议": "给出具体的下一步建议"
+}
+
+不要输出 JSON 以外的任何内容。
+"""
+            },
+            {
+                "role": "user",
+                "content": request.question
+            }
+        ]
+    )
+
+    # AI 返回的是 JSON 字符串
+    result = json.loads(response.choices[0].message.content)
+
+    # 返回给前端
+    return result
--- test_llm.py
+++ test_llm.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -20,15 +21,34 @@ client = OpenAI(
 user_input = input("请输入你的问题：")
 
 response = client.chat.completions.create(
-    model="deepseek-v4-flash",
-    messages=[
-        {
-            "role": "user",
-            "content": user_input
-        }
+model="deepseek-v4-flash",
+messages=[
+    {
+    "role": "system",
+    "content": """
+你是一名专业的校园求职顾问。
+
+当用户询问求职问题时，请严格按照以下 JSON 格式回答：
+
+{
+    "适合岗位": ["岗位1", "岗位2", "岗位3"],
+    "原因": "简要说明为什么适合",
+    "技能缺口": ["技能1", "技能2", "技能3"],
+    "建议": "给出具体的下一步建议"
+}
+
+不要输出 JSON 以外的任何内容。
+"""
+    },
+    {
+        "role": "user",
+        "content": user_input
+    }
     ]
 )
-
-
 # 打印 AI 回复
-print(response.choices[0].message.content)
+result = json.loads(response.choices[0].message.content)
+
+print("适合岗位：", result["适合岗位"])
+print("技能缺口：", result["技能缺口"])
+print("建议：", result["建议"])
