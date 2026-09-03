939f57ac944721f2afd46c52c93775352bf07015
zzy050711 <1346586820@qq.com>
2026-09-03 09:39:25 +0800

    增加简历上传功能，提升简历解析能力。

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import json
+import shutil
 
 from dotenv import load_dotenv
 from openai import OpenAI
-from fastapi import FastAPI
+from fastapi import FastAPI,UploadFile,File
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+from utils.pdf_reader import read_pdf
 
 # 读取 .env
 load_dotenv()
@@ -67,7 +69,55 @@ def analyze(request: UserRequest):
             }
         ]
     )
+# 创建 /analyze_resume 接口
+@app.post("/analyze_resume")
+async def analyze_resume(
+        file: UploadFile = File(...)
+):
 
+    # 保存上传文件
+    file_path = f"temp_{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(
+            file.file,
+            buffer
+        )
+
+
+    # 读取PDF文字
+    resume_text = read_pdf(file_path)
+
+
+    # 调用AI
+    response = client.chat.completions.create(
+        model="deepseek-v4-flash",
+        messages=[
+            {
+                "role": "system",
+                "content": """
+你是一名专业校园招聘顾问。
+
+分析学生简历，并返回JSON：
+
+{
+ "用户画像":"",
+ "适合岗位":[],
+ "技能缺口":[],
+ "简历优化建议":"",
+ "学习规划":""
+}
+
+不要输出其他内容。
+"""
+            },
+            {
+                "role": "user",
+                "content": resume_text
+            }
+        ]
+    )
+    
     # AI 返回的是 JSON 字符串
     result = json.loads(response.choices[0].message.content)
 
---
+++ utils/pdf_reader.py
@@ -0,0 +1,16 @@
+from pypdf import PdfReader
+
+
+def read_pdf(file_path):
+
+    reader = PdfReader(file_path)
+
+    text = ""
+
+    for page in reader.pages:
+        page_text = page.extract_text()
+
+        if page_text:
+            text += page_text
+
+    return text
